3e23bd2a54d092986535b3b9c474acf04ddc2fbe
Slawek Derabin <slawomirderabin@gmail.com>
2023-04-14 17:23:04 +0200

    problem with link resolved

--- src/components/Features/GenerateCards.js
+++ src/components/Features/GenerateCards.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import {Card, Row, Button, Col} from 'react-bootstrap';
+import { Card, Row, Button, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { getAllPosts } from '../../redux/postsRedux';
+import { Link } from 'react-router-dom';
 
 const GenerateCards = () => {
   const posts = useSelector(getAllPosts);
   console.log(posts);
- 
+
   return (
     <Row>
-    {posts.map((post) => (
-      <Col xs={12} md={6} lg={4} key={post.id} className="my-3">
-        <Card>
-          <Card.Body>
-            <Card.Title>{post.title}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">{post.publishedDate}</Card.Subtitle>
-            <Card.Text className='pt-2 mb-0'><strong>Author:</strong> {post.author}</Card.Text>
-            <Card.Text><strong>Description:</strong> {post.content}</Card.Text>
-            <Button variant="warning" href={`/post/${post.id}`}>Read More</Button>
-          </Card.Body>
-        </Card>
-      </Col>
-    ))}
-  </Row>
-  
+      {posts.map((post) => (
+        <Col xs={12} md={6} lg={4} key={post.id} className="my-3">
+          <Card>
+            <Card.Body>
+              <Card.Title>{post.title}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{post.publishedDate}</Card.Subtitle>
+              <Card.Text className='pt-2 mb-0'><strong>Author:</strong> {post.author}</Card.Text>
+              <Card.Text><strong>Description:</strong> {post.content}</Card.Text>
+              <Link to={`/post/${post.id}`}>
+                <Button variant="warning" >Read More</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+
   )
 };
 export default GenerateCards;
